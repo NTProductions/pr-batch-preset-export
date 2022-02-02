00c1f3d5f6bca2df78892792d7b7ad57119016b2
Nate Lovell <Natelovell99@gmail.com>
2022-02-02 16:00:28 -0700

    Create PR Batch Preset Export.jsx

---
+++ PR Batch Preset Export.jsx
@@ -0,0 +1,24 @@
+﻿main("C:/Users/Natel/Videos");
+
+function main(folderPath) {
+    var sequences = app.project.sequences;
+    
+    var vanillaSequence, qeSequence;
+    var exportPath;
+    for(var s = 0; s < sequences.numSequences; s++) {
+        vanillaSequence = sequences[s];
+        exportPath = folderPath + "/" + sequences[s].name;
+        
+        // export proj
+        vanillaSequence.exportAsProject(exportPath.replace(/\//g, "\\")+".prproj");
+        
+        app.project.openSequence(sequences[s].sequenceID);
+        qeSequence = qe.project.getActiveSequence();
+        
+        // export thumbnail
+        qeSequence.exportFramePNG(0, exportPath.replace(/\//g, "\\"));
+        
+        // export preview video
+        app.encoder.encodeSequence(sequences[s], folderPath+"/"+sequences[s].name+".mp4", "C:\\Users\\Natel\\Documents\\Adobe\\Adobe Media Encoder\\15.0\\Presets\\Premiere FX Export.epr", 1, 0);
+        }
+    }
